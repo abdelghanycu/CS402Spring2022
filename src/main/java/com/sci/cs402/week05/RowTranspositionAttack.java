8547b9812dfa82542f1a5ba0eba6bf0c29303ab3
abdelghanysd <aabdelghany@singledigits.com>
2022-03-28 13:17:16 +0200

    Added RT attack

--- src/main/java/com/sci/cs402/week05/RowTranspositionAttack.java
+++ src/main/java/com/sci/cs402/week05/RowTranspositionAttack.java
@@ -1,5 +1,14 @@
 package com.sci.cs402.week05;
 
+import java.io.File;
+
 public class RowTranspositionAttack {
 
+  // the cipherTextFile should contain the cipher text needed to be decrypted.
+  // the result should be all the possible combination of the plaintext:
+  // that can be obtained by row transposition of length 2 -> cipher.length
+  // see the example of the unit test
+  public File attack(File cipherTextFile) throws Exception {
+    return null;
+  }
 }
---
+++ src/test/java/com/sci/cs402/week05/RowTranspositionAttackTest.java
@@ -0,0 +1,9 @@
+package com.sci.cs402.week05;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class RowTranspositionAttackTest {
+
+}
